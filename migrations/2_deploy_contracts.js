24d7cf3e1de1712dbf8903609fad3c15da3b9c3c
zero.qn <zero.qn@users.noreplay.github.com>
2017-10-14 19:51:34 +0800

    CHANGE: update migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,27 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const ANTToken = artifacts.require('ANTToken');
+const USDToken = artifacts.require('USDToken');
+const oraclizeLib = artifacts.require('oraclizeLib');
+const Payroll = artifacts.require('Payroll');
+const PayrollDB = artifacts.require('PayrollDB');
+
+async function deploy(deployer) {
+  let db;
+
+  await deployer.deploy([
+    ANTToken,
+    USDToken,
+    oraclizeLib,
+    PayrollDB,
+  ]);
+
+  deployer.link(oraclizeLib, Payroll);
+  await deployer.deploy(Payroll,PayrollDB.address, ANTToken.address,
+    USDToken.address);
+
+  db = await PayrollDB.deployed();
+  await db.setAllowedContract([Payroll.address]);
+}
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.then(() => deploy(deployer));
 };
